8a221b18e22731cf0d6d942addb356c659ad530c
EngPeterAtef <peter_atef@stud.cu.edu.eg>
2023-07-20 17:16:44 +0300

    use my own keys and I hide sidebar

--- blogWriter_main.py
+++ blogWriter_main.py
@@ -5,6 +5,7 @@ from langchain.utilities import (
 )
 from langchain.tools import WikipediaQueryRun, DuckDuckGoSearchRun
 from langchain import OpenAI
+from dotenv import load_dotenv
 
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
@@ -20,34 +21,35 @@ import os
 
 
 def main():
+    load_dotenv()
     keys_flag = False
 
     st.set_page_config(
         page_title="Blog Writer Agent", page_icon=":message:", layout="wide"
     )
     st.title("Blog Writer Agent: Write a blog about any topic")
-    with st.sidebar:
-        st.subheader("Enter the required keys")
+    # with st.sidebar:
+    #     st.subheader("Enter the required keys")
 
-        st.write("Please enter your OPENAI API KEY")
-        OPENAI_API_KEY = st.text_input("OPENAI API KEY", type="password")
+    #     st.write("Please enter your OPENAI API KEY")
+    #     OPENAI_API_KEY = st.text_input("OPENAI API KEY", type="password")
 
-        st.write("Please enter your Google API KEY")
-        GOOGLE_API_KEY = st.text_input("GOOGLE API KEY", type="password")
+    #     st.write("Please enter your Google API KEY")
+    #     GOOGLE_API_KEY = st.text_input("GOOGLE API KEY", type="password")
 
-        st.write("Please enter your Google CX KEY")
-        GOOGLE_CX_KEY = st.text_input("GOOGLE CX KEY", type="password")
+    #     st.write("Please enter your Google CX KEY")
+    #     GOOGLE_CX_KEY = st.text_input("GOOGLE CX KEY", type="password")
 
-        if OPENAI_API_KEY and GOOGLE_API_KEY and GOOGLE_CX_KEY:
-            os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-            os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
-            os.environ["GOOGLE_CSE_ID"] = GOOGLE_CX_KEY
-            keys_flag = True
-        else:
-            # warning message
-            st.warning("Please enter your API KEY first", icon="⚠")
-            keys_flag = False
-
+    #     if OPENAI_API_KEY and GOOGLE_API_KEY and GOOGLE_CX_KEY:
+    #         os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+    #         os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+    #         os.environ["GOOGLE_CSE_ID"] = GOOGLE_CX_KEY
+    #         keys_flag = True
+    #     else:
+    #         # warning message
+    #         st.warning("Please enter your API KEY first", icon="⚠")
+    #         keys_flag = False
+    keys_flag = True
     if keys_flag:
         # search engines
         wiki = WikipediaAPIWrapper()
