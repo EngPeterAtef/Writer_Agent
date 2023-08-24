8f83e8d0bb27c5324353f47bf0b67348f5522433
EngPeterAtef <peter_atef@stud.cu.edu.eg>
2023-08-24 17:04:10 +0300

    hide env variables

--- qdrant_db.py
+++ qdrant_db.py
@@ -11,6 +11,7 @@ from langchain.text_splitter import (
 from langchain.document_loaders import (
     PyMuPDFLoader,
 )
+
 load_dotenv()
 client = QdrantClient(
     os.getenv("QDRANT_HOST"),
@@ -18,8 +19,7 @@ client = QdrantClient(
 )
 
 
-
-embeddings = OpenAIEmbeddings()
+embeddings = HuggingFaceEmbeddings()
 
 vectorStore_openAI = Qdrant(
     client=client,
@@ -28,35 +28,48 @@ vectorStore_openAI = Qdrant(
 )
 
 text_splitter = RecursiveCharacterTextSplitter(
-            separators=[
-                ".",
-                "\n",
-                "\t",
-                "\r",
-                "\f",
-                "\v",
-                "\0",
-                "\x0b",
-                "\x0c",
-                "\n\n",
-                "\n\n\n",
-            ],
-            chunk_size=1000,
-            chunk_overlap=200,
-        )
-# vectors_config = models.VectorParams(size=1536, distance=models.Distance.COSINE)
-# client.recreate_collection(
-#     collection_name=os.getenv("QDRANT_COLLECTION_NAME"),
-#     vectors_config=vectors_config,
-# )
+    separators=[
+        ".",
+        "\n",
+        "\t",
+        "\r",
+        "\f",
+        "\v",
+        "\0",
+        "\x0b",
+        "\x0c",
+        "\n\n",
+        "\n\n\n",
+    ],
+    chunk_size=1000,
+    chunk_overlap=200,
+)
+# 1536 open ai embeddings
+vectors_config = models.VectorParams(size=768, distance=models.Distance.COSINE)
+client.recreate_collection(
+    collection_name=os.getenv("QDRANT_COLLECTION_NAME"),
+    vectors_config=vectors_config,
+)
 print("collection created")
 bible_loader = PyMuPDFLoader("D:/projects/AI projects/privateGPT/test_docs/bible.pdf")
-quran_loader = PyMuPDFLoader("D:/projects/AI projects/privateGPT/test_docs/Holy-Quran-English.pdf")
+quran_loader = PyMuPDFLoader(
+    "D:/projects/AI projects/privateGPT/test_docs/Holy-Quran-English.pdf"
+)
 print("loaders created")
 bible_docs = bible_loader.load_and_split(text_splitter=text_splitter)
 quran_docs = quran_loader.load_and_split(text_splitter=text_splitter)
+# print(bible_docs[0])
+# print(quran_docs[0])
 print("docs loaded")
-vectorStore_openAI.add_documents(bible_docs)
+# vectorStore_openAI.from_documents(bible_docs,embedding=embeddings)
+Qdrant.from_documents(
+    bible_docs,
+    embedding=embeddings,
+    url=os.getenv("QDRANT_HOST"),
+    api_key=os.getenv("QDRANT_API_KEY"),
+    collection_name=os.getenv("QDRANT_COLLECTION_NAME"),
+    prefer_grpc=True,
+)
 print("bible added")
 vectorStore_openAI.add_documents(quran_docs)
 print("quran added")
